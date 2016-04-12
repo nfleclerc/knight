d977632109d69c70a0441130ce5e9c82043df01c
Nathaniel Leclerc <nfleclerc@live.com>
2016-04-12 02:32:43 -0400

    added empty worldstate class
    Signed-off-by: Nathaniel Leclerc <nfleclerc@live.com>

---
+++ src/gameStates/levels/WorldState.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+package gameStates.levels;
+
+/**
+ * Created by nathaniel on 4/12/16.
+ */
+public class WorldState extends LevelState {
+
+
+    @Override
+    public void init() {
+
+    }
+
+
+}
