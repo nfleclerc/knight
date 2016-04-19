6142ecbcae678f0e5e468387083d4aee333d363a
Nathaniel Leclerc <nfleclerc@live.com>
2016-04-19 01:49:53 -0400

    filename changes
    Signed-off-by: Nathaniel Leclerc <nfleclerc@live.com>

---
+++ src/entity/enemies/Moose.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+package entity.enemies;
+
+import effects.Animation;
+import entity.Player;
+import tileMap.TileMap;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by nathaniel on 4/14/16.
+ */
+public class Moose extends Enemy{
+
+    public Moose(TileMap tm, Player player) {
+        super(tm, player);
+
+        this.dropType = null;
+
+        moveSpeed = 0.3;
+        maxSpeed = 4.0;
+        fallSpeed = 0.8;
+        maxFallSpeed = 1.8;
+
+        width = 90;
+        height = 60;
+        cWidth = 80;
+        cHeight = 40;
+
+        health = maxHealth = 30;
+        xpWorth = 80;
+
+        //loadSprites
+        try{
+            BufferedImage spriteSheet = ImageIO.read(
+                    getClass().getResourceAsStream(
+                            "/sprites/enemies/moose.gif"
+                    )
+            );
+
+            sprites = new BufferedImage[5];
+            for (int i = 0; i < sprites.length; i++) {
+                sprites[i] = spriteSheet.getSubimage(
+                        i * width,
+                        0,
+                        width,
+                        height
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        animation = new Animation();
+        animation.setFrames(sprites);
+        animation.setDelay(100);
+
+        right = true;
+        facingRight = true;
+    }
+
+}
