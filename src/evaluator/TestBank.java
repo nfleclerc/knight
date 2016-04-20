b5ae10946c79427d927eceedc6da29e28c48d612
Nathaniel Leclerc <nfleclerc@live.com>
2016-04-20 14:30:59 -0400

    started parsing testfiles
    Signed-off-by: Nathaniel Leclerc <nfleclerc@live.com>

--- src/evaluator/TestBank.java
+++ src/evaluator/TestBank.java
@@ -4,7 +4,13 @@
 
 package evaluator;
 
+import entity.Player;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by nathaniel on 4/16/16.
@@ -12,12 +18,37 @@ import java.util.HashMap;
 public class TestBank {
 
     private final HashMap<String, String> testAnswerMap;
+    private String intro;
 
     public TestBank(){
         this.testAnswerMap = new HashMap<>();
 
+        try (BufferedReader br = new BufferedReader(new FileReader("res/tests/tests.csv"))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] tokens = line.split(",");
+                testAnswerMap.put(tokens[0], tokens[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        try {
+            intro = IOUtils.toString(new InputStreamReader(new FileInputStream("res/tests/intro.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public String getTest(Player player){
+        return player.getXP() == 0 ? intro : getRandomTest();
+    }
+
+    private String getRandomTest() {
+       return  ((List<String>) testAnswerMap.keySet())
+               .get(new Random().nextInt(testAnswerMap.size()));
+    }
+
 
 }
