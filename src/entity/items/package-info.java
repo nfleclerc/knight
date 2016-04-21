baa6201b29aed63aa952e5941f4e4531465ea00f
Nathaniel Leclerc <nfleclerc@live.com>
2016-04-21 14:05:00 -0400

    cleaned up ant
    Signed-off-by: Nathaniel Leclerc <nfleclerc@live.com>

---
+++ src/entity/items/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+/**
+ * Created by nathaniel on 4/21/16.
+ *
+ * This package contains a way for creating new Items. With the removal
+ * of the crafting mechanic items no longer serve a purpose and are really only
+ * included as novelty things, and only for an aesthetic purpose. Since they no
+ * longer hold a purpose, no new items have been made past the original two items
+ * made to drop from the Wasp and Spider. These items can still be collected and
+ * otherwise function correctly. You just can't do anything with them once you
+ * have them.
+ *
+ */
+
+@Deprecated
+package entity.items;
